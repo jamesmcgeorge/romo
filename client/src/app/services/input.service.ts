e80530ff9a9179f1d39ffe29cacebf1ad73830a4
James McGeorge <jimgymn@gmail.com>
2018-09-13 07:28:47 -0700

    Update input.service.ts

--- client/src/app/services/input.service.ts
+++ client/src/app/services/input.service.ts
@@ -3,7 +3,7 @@ import { Action, ActionType } from '../actions/action';
 import { Creature } from '../objects/creature';
 import { Subject } from 'rxjs/Subject';
 import { Player } from '../objects/player';
-import { ActionService, KeyboardDownAction, KeyboardUpAction } from './action.service';
+import { ActionService, KeyboardDownAction } from './action.service';
 import { Injectable } from '@angular/core';
 import { openDoors, closedDoors } from './sprite.service';
 import { Lootable } from '../objects/loot.bag';
@@ -11,33 +11,38 @@ const Keys = ex.Input.Keys;
 
 @Injectable()
 export class InputService {
-    showRadialSubject = new Subject<boolean>();
-    menuDataSubject = new Subject<any>();
-    tempPlayer: Player;
-    menuOptions: any[];
-    cellClicked: ex.Cell;
-    actorClicked: Creature | Lootable;
-    xDist: number;
-    yDist: number;
-    lastKeyAction: KeyboardDownAction;
-    lastKeyUpAction: KeyboardUpAction;
-    inputAction: KeyboardDownAction;
-    lastActionTime: number;
-    keysDown: ex.Input.Keys[] = [];
+    public showRadialSubject = new Subject<boolean>();
+    public menuDataSubject = new Subject<any>();
+    private tempPlayer: Player;
+    private menuOptions: any[];
+    private cellClicked: ex.Cell;
+    private actorClicked: Creature | Lootable;
+    private xDist: number;
+    private yDist: number;
+    public lastKeyAction: KeyboardDownAction;
+    private inputAction: KeyboardDownAction;
+    private keysDown: ex.Input.Keys[] = [];
 
     constructor(private actionService: ActionService) {}
 
+    /**
+     * Get's an action from the current keyboard input
+     * @param engine The Excalibur.js game engine instance
+     */
     public getKeyboardInputAction(engine: ex.Engine): Action {
         this.checkKeysDown(engine);
         this.checkPossibleInput();
         const actionToReturn = this.actionService.getKeyBoardDownAction(this.inputAction);
         return actionToReturn;
     }
-
+    /**
+     * Populates this.keysDown with the all current keys being pressed
+     * @param engine The Excalibur.js game engine instance
+     */
     private checkKeysDown(engine: ex.Engine) {
         this.keysDown = engine.input.keyboard.getKeys();
     }
-
+    /** Iterates through the current keys being pressed and then populates this.inputAction */
     private checkPossibleInput() {
         this.inputAction = null;
 
@@ -73,14 +78,22 @@ export class InputService {
             }
         }
     }
-
+    /**
+     * Checks if a key is being pressed
+     * @param key Key to check
+     */
     private isPressed(key: ex.Input.Keys): boolean {
         if (this.keysDown.indexOf(key) >= 0) {
             return true;
         }
         return false;
     }
-
+    /**
+     * Processes left and right mouse clicks
+     * @param event Excalibur.js input pointer event to process
+     * @param engine The Excalibur.js game engine instance
+     * @param player The player object
+     */
     public processMouseInput(event: ex.Input.PointerEvent, engine: ex.Engine, player: Player) {
         if (event.button === 0) {
             this.processLeftClick(event, engine);
@@ -88,28 +101,40 @@ export class InputService {
             this.processRightClick(event, engine, player);
         }
     }
-
+    /**
+     * Processes the left click, currently only logs which cell is clicked for debugging
+     * @param event Excalibur.js input pointer event to process
+     * @param engine The Excalibur.js game engine instance
+     */
     private processLeftClick(event: ex.Input.PointerEvent, engine: ex.Engine) {
         console.log(event);
         const cellClicked = engine.currentScene.tileMaps[0].getCellByPoint(event.worldPos.x, event.worldPos.y);
         console.log(cellClicked);
     }
-
+    /**
+     * Processes the right clicks, this is mostly a contextual input
+     * @param event Excalibur.js input pointer event to process
+     * @param engine The Excalibur.js game engine instance
+     * @param player The player object
+     */
     private processRightClick(event: ex.Input.PointerEvent, engine: ex.Engine, player: Player) {
         this.showRadialSubject.next(true);
         this.tempPlayer = player;
         this.cellClicked = engine.currentScene.tileMaps[0].getCellByPoint(event.worldPos.x, event.worldPos.y);
         const fogCellClicked = engine.currentScene.tileMaps[1].getCellByPoint(event.worldPos.x, event.worldPos.y);
+        // If the player is clicking in fog, don't let them see any menu
         if (fogCellClicked.sprites.length > 0) {
             return;
         }
 
         this.actorClicked = null;
+        // check if we're right clicking on a monster
         for (const actor of player.gmService.monsters) {
             if (actor.x === this.cellClicked.x && actor.y === this.cellClicked.y) {
                 this.actorClicked = actor;
             }
         }
+        // If we didn't find a monster, check if it's another player
         if (!this.actorClicked) {
             for (const actor of player.gmService.otherPlayers) {
                 if (actor.x === this.cellClicked.x && actor.y === this.cellClicked.y) {
@@ -117,7 +142,7 @@ export class InputService {
                 }
             }
         }
-
+        // If we still haven't clicked something, check if there is a lootable object
         if (!this.actorClicked) {
             for (const lootable of player.gmService.lootables) {
                 if (lootable.x === this.cellClicked.x && lootable.y === this.cellClicked.y) {
@@ -143,7 +168,7 @@ export class InputService {
                 this.getMenuOptions(this.cellClicked.sprites[1].spriteId);
             }
         }
-
+        // organise where to show the popup menu
         const left = event.pagePos.x - 50;
         const right = window.innerWidth - event.pagePos.x + 50;
         const top = event.pagePos.y;
@@ -160,8 +185,11 @@ export class InputService {
         };
         this.menuDataSubject.next(data);
     }
-
-    processMenuClick(i: number) {
+    /**
+     * Process the menu item that was selected
+     * @param i The index of the menu item
+     */
+    public processMenuClick(i: number) {
         const payload = {
             menuOption: this.menuOptions[i],
             data: {
@@ -177,6 +205,7 @@ export class InputService {
         }
         this.tempPlayer = undefined;
     }
+    /** Get the menu options for when an Actor is clicked */
     private getActorMenuOptions() {
         if ((<Lootable>this.actorClicked).isLootable) {
             if (this.xDist <= 32 && this.yDist <= 32) {
@@ -191,6 +220,10 @@ export class InputService {
             }
         }
     }
+    /**
+     * Get the menu options based on the sprite clicked
+     * @param spriteId The ID of the sprite being clicked
+     */
     private getMenuOptions(spriteId: number) {
         if (openDoors.indexOf(spriteId) >= 0) {
             this.menuOptions.push({name: 'Close', aType: ActionType.closeDoor});
@@ -198,6 +231,7 @@ export class InputService {
         if (closedDoors.indexOf(spriteId) >= 0) {
             this.menuOptions.push({name: 'Open', aType: ActionType.openDoor});
         }
+        // Want to change this to an array of stair sprites to allow more variety, same as the above door options
         if (spriteId === 1061) {
             const x = this.cellClicked.x;
             const y = this.cellClicked.y;
@@ -206,6 +240,7 @@ export class InputService {
                 this.menuOptions.push({name: 'Up', aType: ActionType.changeMap, data: nextMap});
             }
         }
+        // Want to change this to an array of stair sprites to allow more variety
         if (spriteId === 1063) {
             const x = this.cellClicked.x;
             const y = this.cellClicked.y;
