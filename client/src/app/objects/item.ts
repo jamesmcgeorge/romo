2d40a259c777c78eea1fb0a88caccea4d18c706b
James McGeorge <jimgymn@gmail.com>
2018-09-13 06:48:55 -0700

    Create item.ts

---
+++ client/src/app/objects/item.ts
@@ -0,0 +1,84 @@
+import {Material} from './material';
+
+export interface IQuality {
+    name: string;
+    index: number;
+    valueMult: number;
+}
+
+export enum ItemType {
+    armor,
+    weapon,
+    consumable,
+    junk,
+    bag,
+    ring,
+}
+
+export interface ISocketItem {
+    /** The material index of the item */
+    m: number;
+    /** The quality index of the item */
+    q: number;
+    /** The primary type of item */
+    t: number;
+    /** The weapon type of the item, if any */
+    w?: number;
+    /** The armor type of the item, if any */
+    a?: number;
+    /** The subtype of the item, if any */
+    s?: number;
+    /** The enhancement of the item, if any (tech or magic) */
+    e?: any;
+    /** Total bag slots of the item, if any */
+    b?: number;
+}
+
+export const QualitiesArray: IQuality[] = [
+    {name: 'poor', index: 0, valueMult: 0.8},
+    {name: 'normal', index: 1, valueMult: 1},
+    {name: 'good', index: 2, valueMult: 1.1},
+    {name: 'rubbish', index: 3, valueMult: 0.6},
+    {name: 'great', index: 4, valueMult: 1.2},
+    {name: 'awesome', index: 5, valueMult: 1.3},
+];
+
+export class Item {
+    public weight: number;
+    public value: number;
+
+    constructor(public type: ItemType, public subType: number, public material: Material, protected baseWeight: number, protected baseValue: number, public quality: IQuality, public techMagic?: any) {
+        this.calculateWeight();
+        this.caclculateValue();
+        if (techMagic) {
+            // do something about the enhancement? do all items need this? Maybe only weapons?
+        }
+    }
+
+    get description() {
+        const str = `
+Weight: ${this.weight.toFixed(2)}
+Value: ${this.value.toFixed(2)}
+Quality: ${this.quality.name}
+Material: ${this.material.name}
+`;
+    return str;
+    }
+
+    calculateWeight() {
+        this.weight = this.baseWeight;
+        this.weight *= this.material.weightMult;
+        if (this.techMagic) {
+            this.weight *= this.techMagic.weightMult;
+        }
+    }
+
+    caclculateValue() {
+        this.value = this.baseValue;
+        this.value *= this.material.valueMult;
+        if (this.techMagic) {
+            this.weight *= this.techMagic.valueMult;
+        }
+        this.value = this.value * this.quality.valueMult;
+    }
+}
